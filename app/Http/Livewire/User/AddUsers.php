3036cbaa640ea3d27d540e1f7982531d63e7144f
AshDest <ashuzamaheshe4@gmail.com>
2023-02-19 07:00:11 +0100

    ash commit

---
+++ app/Http/Livewire/User/AddUsers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class AddUsers extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.add-users');
+    }
+}
